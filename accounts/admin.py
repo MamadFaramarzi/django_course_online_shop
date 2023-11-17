73c6d403b24788ca4bb6e3aa3eff1dd4da66fbb5
Mamadfaramarzi <mohammadfaramarzi.f78@gmail.com>
2023-11-17 17:36:27 +0330

    add CustomUser to django project

---
+++ accounts/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+from .forms import UserCreationForm, UserChangeForm
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = UserCreationForm
+    form = UserChangeForm
+    list_display = ('email', 'username')
+
+#
+# admin.site.register(CustomUser, CustomUserAdmin)
