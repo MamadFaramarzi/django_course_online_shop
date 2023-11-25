7871f3e44d0b86bfcbe38c5da9f394535c406420
Mamadfaramarzi <mohammadfaramarzi.f78@gmail.com>
2023-11-25 22:50:56 +0330

    add emailconfirm

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,17 @@ AUTHENTICATION_BACKENDS = [
     # Django allauth
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mohammadfaramarzi.f78@gmail.com'
+EMAIL_HOST_PASSWORD = "tdsx viic nlyz jwbk"
+
+
+
+
 
 
 # Internationalization
