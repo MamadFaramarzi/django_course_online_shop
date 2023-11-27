c584068de7262a966052fc030eb384b73eb56c97
Mamadfaramarzi <mohammadfaramarzi.f78@gmail.com>
2023-11-27 10:43:23 +0330

    create list and detail view for product app

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path("", views.ProductListView.as_view(), name='product_list'),
+    path("<int:pk>/", views.ProductDetailView.as_view(), name='product_detail'),
+]
