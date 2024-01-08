260f84b62cba7d9dd89c29e57e4af70ad16918b7
Mamadfaramarzi <mohammadfaramarzi.f78@gmail.com>
2024-01-08 21:04:02 +0330

    add short desctiption to model and some translation

--- products/models.py
+++ products/models.py
@@ -9,14 +9,15 @@ from ckeditor.fields import RichTextField
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    description = RichTextField()
-    price = models.PositiveIntegerField(default=0)
-    active = models.BooleanField(default=True)
-    image = models.ImageField(verbose_name=_('product image'), upload_to='product/product_cover/', blank=True, )
+    title = models.CharField(verbose_name=_('product title'), max_length=100)
+    description = RichTextField(verbose_name=_("product description"))
+    short_description = models.TextField(_('short description'), blank=True)
+    price = models.PositiveIntegerField(_("price"), default=0)
+    active = models.BooleanField(verbose_name=_('product active'), default=True)
+    image = models.ImageField(_('product image'), upload_to='product/product_cover/', blank=True, )
 
     datetime_created = models.DateTimeField(_('date time of creation'), default=timezone.now)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    datetime_modified = models.DateTimeField(_('date time of last edit'), auto_now=True)
 
     def __str__(self):
         return self.title
@@ -38,16 +39,16 @@ class Comment(models.Model):
         ('1', _('Good')),
         ('1', _('Perfect')),
     ]
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="comments")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="comments", verbose_name=_("product"))
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
-           related_name="comments", verbose_name=_("Comment Author"))
+                               related_name="comments", verbose_name=_("Comment Author"))
     body = models.TextField(verbose_name=_("Comment Text"))
     stars = models.CharField(max_length=10, choices=PRODUCT_CHOICES, verbose_name=_('Your Score'))
 
-    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_created = models.DateTimeField(_('date time of comment creation'), auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(_("valid"), default=True)
 
     # manager
     objects = models.Manager()
